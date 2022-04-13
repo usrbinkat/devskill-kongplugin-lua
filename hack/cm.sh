3ed1f2001e196bff427ea143f3611dfca852cf68
usrbinkat <usrbinkat@braincraft.io>
2022-04-13 11:51:26 -0700

    update hack testing script
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- hack/cm.sh
+++ hack/cm.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -x
 kubectl delete configmap -nkong rolescopes
 kubectl create configmap -nkong --from-file=kong/plugins/rolescopes rolescopes --dry-run=client -oyaml | kubectl apply -nkong -f -
+kubectl delete po -nkong --selector=app=dataplane-kong --grace-period=0
+sleep 2
+kubectl wait --for=condition=ready -nkong -l app=dataplane-kong po
+kubectl logs -nkong -l app=dataplane-kong -f
